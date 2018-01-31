f374d0ba68340765bba5c405decf18e43c105b90
Liam Holmes <liam.holmes@europlusdirect.com>
2018-01-31 10:49:46 +0000

    Added some basic descriptions
    I've added some basic descriptions to describe what things do in a bit more detail :)

--- scripts/mp/FloorIsLava.lua
+++ scripts/mp/FloorIsLava.lua
@@ -1,9 +1,15 @@
 util.print("Starting Floor Is Lava Gamemode...")
 util.chatPrint("Starting Floor Is Lava Gamemode...")
 
+-- Sets jump height to 800 (duh) so that we can jump heigher, although fall damage is still the same
+-- may want to look into falldamage if it starts becoming a problem
 gsc.setdvar("jump_height", "800")
+-- Sets gravity lower, default I think is 800, so 200 is not too low but low enough so you fly around
+-- which makes the knife fights more interesting in my opinion
 gsc.setdvar("g_gravity", "200")
-
+-- The Fun bit, adjusts the player's melee range so they can knife enemies and breakable objects (e.g windows)
+-- from pretty much anywhere on the map. Interestingly if you hit a wall near a player, the player will hear the
+-- knife collision even if the enemy is nowhere near.
 gsc.setdvar("player_meleeRange", "10000000")
 
 util.chatPrint("Floor Is Lava Gamemode Loaded, get Jumping!")

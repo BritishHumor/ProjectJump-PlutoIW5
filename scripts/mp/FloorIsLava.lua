813db5890ccb9061a5d78ba364f61c15c0f21ce8
Liam Holmes <liam.holmes@europlusdirect.com>
2018-01-31 10:43:39 +0000

    Create FloorIsLava.lua

---
+++ scripts/mp/FloorIsLava.lua
@@ -0,0 +1,10 @@
+util.print("Starting Floor Is Lava Gamemode...")
+util.chatPrint("Starting Floor Is Lava Gamemode...")
+
+gsc.setdvar("jump_height", "800")
+gsc.setdvar("g_gravity", "200")
+
+gsc.setdvar("player_meleeRange", "10000000")
+
+util.chatPrint("Floor Is Lava Gamemode Loaded, get Jumping!")
+util.print("Floor Is Lava Gamemode Loaded, get Jumping!")
